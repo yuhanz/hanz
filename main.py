745a14808deb1c6ff700ae2da57cf5b47d8ccd82
Zhang <yuhanz@gmail.com>
2022-06-17 02:25:26 -0700

    able to define multiple pipelines  and combine result

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import torch
 import re
 from functools import partial
 import pdb
+import sys
 
 class Custom(nn.Module):
     def __init__(self, fn, no_argument = False):     # Let the fn be a torch function. for example: torch.sin
@@ -16,11 +17,15 @@ class Custom(nn.Module):
         return r
 
 class CustomCombine(nn.Module):
-    def __init__(self, fn):     # Let the fn be a torch function. for example: torch.sin
+    def __init__(self, fn, fn1, fn2):     # Let the fn be a torch function. for example: torch.sin
         super().__init__()
         self.fn = fn
+        self.fn1 = fn1
+        self.fn2 = fn2
     def forward(self, x):
-        r = self.fn(x)
+        r1 = self.fn1(x)
+        r2 = self.fn2(x)
+        r = self.fn(r1, r2)
         return r
 
 
@@ -31,13 +36,15 @@ class Add(nn.Module):
     def forward(self, x):
         return x+ self.model(x)
 
-f = open("./example.hanz","r")
+file_name = sys.argv[1]
+f = open(file_name, "r")
 lines = f.readlines()
 print(lines)
 
 dim = int(lines[0])
 lines.pop(0)
 modules = [None]
+dims = [dim]
 
 for line in lines:
   [operators, configs] = (line.split("...") + [''])[0:2]
@@ -49,14 +56,20 @@ for line in lines:
   config_list = list(map(lambda s: s.strip(), config_list))
 
   new_modules = []
-  # print(config_list)
-  # print(operators)
+  new_dims = []
   z = zip(operators, config_list)
   commands = list(z)
   # for operator, config in zip(operators, config_list):
+  num_commands = len(commands)
+  num_modules = len(modules)
+  if num_commands > num_modules:
+      for i in range(0, num_commands - num_modules):
+          modules.append(modules[-1])
+          dims.append(dims[-1])
   for operator, config in commands:
       # pdb.set_trace()
       m = modules.pop(0)
+      dim = dims.pop(0)
       value_1 = float(re.sub(r"\s", "", config) or "0")
       if operator == '森':
         output_dim = int(value_1)
@@ -86,19 +99,21 @@ for line in lines:
         new_module = Custom(partial(torch.linspace, start, end, steps), no_argument = True)
       elif operator == '+':
           m2 = modules.pop(0)
-          m = m + m2
+          m = CustomCombine(torch.add, m, m2)
       elif operator == '艹':
           m2 = modules.pop(0)
           m = torch.concat((m, m2), 0)
       else:
         raise Exception('Unrecognized operator: {}'.format(operator))
-      if m:
+      if m and operator != '川':
         m = nn.Sequential(m, new_module)
       else:
         m = new_module
       # pdb.set_trace()
       new_modules.append(m)
+      new_dims.append(dim)
   modules = new_modules
+  dims = new_dims
 
 print(modules)
 print(modules[0](torch.Tensor([1,2])))
