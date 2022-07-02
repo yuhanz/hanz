3aa3ffada6c21fb127c5602045e09d2f25a22209
Zhang <yuhanz@gmail.com>
2022-07-02 14:32:23 -0700

    document how to use make file to test

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # hanz
 Neural Network DSL using Hanzi (Chinese Character) to build Neural Network in PyTorch
+
+## To install dependencies
+make setup
+
+## To Run test cases
+make test
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import hanz
+import torch.nn as nn
+import torch
 
 def pipelineToString(pipeline):
     return ",".join(map(lambda m: m._get_name(), pipeline))
@@ -6,11 +8,17 @@ def pipelineToString(pipeline):
 def test_example():
     modules = hanz.parseHanz('examples/example.hanz')
     assert len(modules) == 1
+    assert isinstance(modules[0], nn.Module)
     assert pipelineToString(modules[0]) == "Linear,LeakyReLU,ReLU,Sigmoid,Softplus,Softmax,Sin"
+    input = torch.Tensor([1,2])
+    output = modules[0](input)
+    assert len(output) == 4
 
 def test_multiple_module_pipeliness():
     modules = hanz.parseHanz('examples/example2a.hanz')
     assert len(modules) == 2
+    assert isinstance(modules[0], nn.Module)
+    assert isinstance(modules[1], nn.Module)
     assert pipelineToString(modules[0]) == "Sin,Linear,ReLU"
     assert pipelineToString(modules[1]) == "Linear,LeakyReLU,ReLU,Sigmoid,Softplus,Softmax,Sin"
 
@@ -19,4 +27,4 @@ def test_combine_multiple_module_pipes():
     assert len(modules) == 1
     assert isinstance(modules[0], hanz.CustomCombine)
     assert pipelineToString(modules[0].fn1) == "Sin,Linear,ReLU"
-    assert pipelineToString(modules[0].fn2) == "Linear,LeakyReLU,ReLU,Sigmoid,Softplus,Softmax,Sin"e
+    assert pipelineToString(modules[0].fn2) == "Linear,LeakyReLU,ReLU,Sigmoid,Softplus,Softmax,Sin"
