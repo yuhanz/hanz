3aa8a2b73f182f808bd54c89e31344708a1da6b0
Zhang <yuhanz@gmail.com>
2022-07-04 02:47:18 -0700

    nerf: to rgb image and optimize the loss function

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import torch
 import hanz
 import sys
 import pdb
+import numpy as np
 
 def get_position_inputs(W, H):
     xs, ys = torch.meshgrid(torch.linspace(0, H-1, H), torch.linspace(0, W-1, W))
@@ -21,14 +22,25 @@ def get_position_inputs(W, H):
 file_name = "examples/example-nerf.hanz"
 modules = hanz.parseHanz(file_name)
 
-nerf_module = modules[0]
-
+model = modules[0]
 
 image = Image.open("/Users/yzhang/Downloads/puppy2.png")
 W, H = image.size
 
 x = get_position_inputs(W, H)
 # pdb.set_trace()
-result = nerf_module(x)
 
-print(result.shape)
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+model.to(device)
+
+result_image = model(x)
+learning_rate = 0.0002
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, betas=(0.5, 0.999))
+
+target_image = torch.Tensor(np.asarray(image).flatten())
+# pdb.set_trace()
+loss = torch.nn.L1Loss()(result_image.flatten(), target_image )
+print("loss", loss)
+optimizer.zero_grad()
+loss.backward()
+optimizer.step()
