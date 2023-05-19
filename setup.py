777c7172490fa7b7beb687b4326b5b01a8eeec17
Zhang <yuhanz@gmail.com>
2023-05-19 00:06:04 -0700

    setup files for pypi

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='hanz',
+    version='0.1',
+    license='Apache 2.0',
+    author="Yuhan Zhang",
+    author_email='yuhanz@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': './'},
+    url='https://github.com/yuhanz/hanz',
+    keywords='Neural Network DSL',
+    install_requires=[
+          'torch',
+          'torchvision'
+      ],
+
+)
